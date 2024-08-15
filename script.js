0170fd7f165d9a2632f0c310296d157c0bc014bf
Sanjaya_023 <sanjaya20004@gmail.com>
2024-08-15 08:35:22 +0700

    Membuat Object Dwngan Constructor Function

--- script.js
+++ script.js
@@ -18,3 +18,26 @@
 
 // 	return color;
 // }
+
+
+// Membuat Object Dwngan Constructor Function
+function Color(r, g, b) {
+	this.r = r;
+	this.g = g;
+	this.b = b;
+}
+
+Color.prototype.rgb = function () {
+	const { r, g, b } = this;
+	return `rgb(${r}, ${g}, ${b})`;
+};
+
+Color.prototype.hex = function () {
+	const { r, g, b } = this;
+	return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+};
+
+Color.prototype.rgba = function (a = 1.0) {
+	const { r, g, b } = this;
+	return `rgba(${r}, ${g}, ${b}, ${a})`;
+};
